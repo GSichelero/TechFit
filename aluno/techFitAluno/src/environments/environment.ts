23b6541fd5ec6319d8e9cdca06ccbab16e5c0266
Brendon Araújo <brendong.araujo@gmail.com>
2022-11-15 18:03:20 -0300

    #ADD - Adcionado firebase aluno e firesotre

--- aluno/techFitAluno/src/environments/environment.ts
+++ aluno/techFitAluno/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'techfit-93e88',
+    appId: '1:146454138892:web:2e7df496a8554f59091e29',
+    storageBucket: 'techfit-93e88.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAwcYtsUIeP9zt3tXVmfA26Eu4dWBxFWtw',
+    authDomain: 'techfit-93e88.firebaseapp.com',
+    messagingSenderId: '146454138892',
+  }
 };
 
 /*
