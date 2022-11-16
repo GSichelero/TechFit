a87fb3da5480f616e53ac19bf9395e7fafa070cd
Brendon Araújo <brendong.araujo@gmail.com>
2022-11-15 21:07:09 -0300

    #FIX - Erro compilacao personal

--- personal/techFitPersonal/src/app/app.module.ts
+++ personal/techFitPersonal/src/app/app.module.ts
@@ -3,22 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import {AngularFireModule } from '@angular/fire/compat';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment';
-import { ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
-
-import {AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule
-    , IonicModule.forRoot()
-    , AppRoutingModule
-    , AngularFireModule.initializeApp(environment.firebase)]]
-  ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ScreenTrackingService,UserTrackingService],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebase)],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
