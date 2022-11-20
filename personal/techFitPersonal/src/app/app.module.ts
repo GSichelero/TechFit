ece171574976193397ce4e3861cec8e400925632
Brendon Araújo <brendong.araujo@gmail.com>
2022-11-20 18:58:34 -0300

    #ADD - Adicionado novo menu lateral

--- personal/techFitPersonal/src/app/app.module.ts
+++ personal/techFitPersonal/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { MenuComponent } from './components/menu/menu.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -9,7 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,MenuComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebase)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
---
+++ personal/techFitPersonal/src/app/components/menu/menu.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'my-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+})
+export class MenuComponent implements OnInit {
+
+  user = {
+    name: 'Brendon Araújo',
+    age: 21,
+    email: 'brendong.araujo@gmail.com'
+  }
+
+  Pages = [
+    { title: 'Home', url: '/home' },
+    { title: 'Listagem Semanas', url: '/listagem/semanas' }
+  ];
+  constructor(private route: Router) { 
+  }
+  nextpage(url) {
+    this.route.navigate([url]);
+  }
+
+  ngOnInit() {}
+
+}
